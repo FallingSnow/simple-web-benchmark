f5201a4fe60d5d02b960c2857d369503777800eb
Ayrton Sparling <ayrton@sparling.us>
2020-10-12 16:17:00 -0700

    nodejs/fastify remove extra comments

--- nodejs/fastify/main.js
+++ nodejs/fastify/main.js
@@ -31,41 +31,18 @@ if (cluster.isMaster) {
     // Workers can share any TCP connection
     // In this case it is an HTTP server
 
-    const fastify = require('fastify')();
+    const app = require('fastify')();
 
-    fastify.get('/', async (request, reply) => {
+    app.get('/', async (request, reply) => {
       reply.status(200);
       return "Hello World!";
     });
-    fastify.get('/greeting/:name', async (request, reply) => {
+    app.get('/greeting/:name', async (request, reply) => {
       reply.status(200);
       return `Hello ${request.params.name}`;
     });
 
-    fastify.listen(port);
-    // http.createServer((req, res) => {
-    //     var match;
-    //
-    //     switch (req.url) {
-    //     case "/":
-    //         res.statusCode = 200;
-    //         res.statusMessage = 'OK';
-    //         res.write("Hello World!");
-    //         break;
-    //     default:
-    //         match = greetingRe.exec(req.url);
-    //         if (match) {
-    //             res.statusCode = 200;
-    //             res.statusMessage = 'OK';
-    //             res.write("Hello, " + match[1]);
-    //         } else {
-    //             res.statusCode = 404;
-    //             res.write('Not found');
-    //         }
-    //     }
-    //
-    //     res.end();
-    // }).listen(port);
+    app.listen(port);
 
     console.log(`Worker ${process.pid} started`);
 }
